d5d42eb56a7fb64f199a03d71b6633f0fa3c2326
Tilman Holube <tilman@holube.de>
2025-04-02 16:38:55 +0200

    print api version and core versions

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,18 @@ mod query;
 mod vig_command;
 
 use crate::apis::configuration::Configuration;
-use clap::{Parser, Subcommand};
+use crate::apis::default_api;
+use clap::{crate_name, crate_version, Parser, Subcommand};
+use tokio::runtime::Runtime;
 
 fn main() {
     let args = VigArgs::parse();
     let config = Configuration::new();
+
+    if args.version {
+        show_version();
+        return;
+    }
 
     match args.command {
         Some(VigSubCommands::Index { paths }) => {
@@ -34,10 +41,13 @@ fn main() {
 }
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(about, long_about = None, disable_version_flag = true)]
 pub struct VigArgs {
     #[command(subcommand)]
     command: Option<VigSubCommands>,
+
+    #[arg(short = 'V', long, help = "Print version")]
+    version: bool,
 }
 
 #[derive(Subcommand)]
@@ -60,4 +70,36 @@ enum VigSubCommands {
         )]
         query: Vec<String>,
     },
+}
+
+fn show_version() {
+    let config = Configuration::new();
+    let config_clone = config.clone();
+    let result = default_api::get_version(&config_clone);
+
+    let cli_name = crate_name!();
+    let cli_version = crate_version!();
+    // get the api version from the user agent
+    let user_agent = config.user_agent.unwrap();
+    let cli_api_version = user_agent.split("/").nth(1).unwrap();
+
+    println!("{cli_name} {cli_version}");
+    println!("API version: {cli_api_version}");
+
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async {
+        match result.await {
+            Ok(core_version) => {
+                println!("Service version: {}", core_version.service_version);
+                println!("Service API version: {}", core_version.api_version);
+
+                if core_version.api_version != cli_api_version {
+                    println!("Warning: API version mismatch.");
+                }
+            }
+            Err(e) => {
+                println!("Could not get version of service: {}", e);
+            }
+        }
+    });
 }
